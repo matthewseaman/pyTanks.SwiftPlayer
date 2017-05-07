676a831aece2766760b307ee467d36958f29a912
Matthew Seaman <mattsdeveloper@gmail.com>
2017-05-07 02:15:06 -0400

    Added Starscream as dependency

--- Package.swift
+++ Package.swift
@@ -3,5 +3,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "pyTanks.SwiftPlayer"
+    name: "pyTanks.SwiftPlayer",
+    dependencies: [
+        .Package(url: "https://github.com/daltoniam/Starscream.git", majorVersion: 2)
+    ]
 )
