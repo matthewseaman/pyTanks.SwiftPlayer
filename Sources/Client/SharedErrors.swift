aa56a4cccb5a548f581bafdb810dd233351721f3
Matthew Seaman <mattsdeveloper@gmail.com>
2017-05-07 00:15:03 -0400

    Changed error conditions and conformed to CustomStringConvertable

--- Sources/Client/SharedErrors.swift
+++ Sources/Client/SharedErrors.swift
@@ -8,9 +8,22 @@
 
 
 /// A JSON Serialization Error
-internal enum JSONError: Error {
+internal enum JSONError: Error, CustomStringConvertible {
     /// The JSON data was not valid JSON and could not be parsed
     case notValidJSON
-    /// The JSON data was parsed but it did not match the expected format for this object
-    case invalidFormatForObject
+    /// The value at `keyPath` was missing
+    case missing(keyPath: String)
+    /// The `value` at `keyPath` was invlalid
+    case invalid(keyPath: String, value: Any)
+    
+    var description: String {
+        switch self {
+        case .notValidJSON:
+            return "The received JSON was in an invalid format."
+        case .missing(let keyPath):
+            return "The received JSON is missing \"\(keyPath)\"."
+        case .invalid(let keyPath, let value):
+            return "The received JSON contained \(value), an invlalid value for \(keyPath)."
+        }
+    }
 }
